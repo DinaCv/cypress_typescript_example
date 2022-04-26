f9e28f2028067da480561615c6fc6e5a92c5de9e
Dina Tsveigoren <Dina_Tsveigoren@swissre.com>
2022-04-26 21:40:57 +0300

    Login-logout tests scripted.

---
+++ cypress/integration/specs/login_logout.spec.ts
@@ -0,0 +1,49 @@
+describe('Login/logout test', () => {
+
+    it('Successful login test', () => {
+        cy.visit(Cypress.env('baseUrl'));
+        cy.get('#username').type(Cypress.env('usar'));
+        cy.get('#password').type(Cypress.env('pazz'), {log: false});
+        cy.contains('Login').click();
+        cy.get('#profile > .avatar-block').click();
+        const userName = Cypress.env('usar').replace('.', "-");
+        cy.get('.user-nicename').contains(userName);
+        cy.get('.user-nicename').contains(userName).should((elem) => {
+            expect(elem.text()).to.equal('@' + userName);
+        });
+
+        // assert avatar's name in 'alt' attribute
+        const name = Cypress.env('usar').replace('.', " ");
+        cy.get('#item-header-avatar img').should((elem) => {
+            expect(elem.attr('alt').toLowerCase()).to.contain(name);
+        });
+
+        // assert name in breadcrumb link
+        cy.get('.breadcrumb-extra span[class="active"]').should((elem) => {
+            expect(elem.text().toLowerCase()).to.equal(name);
+        });
+    });
+
+    it('Logout test', () => {
+        cy.get('.logout-button > a').click();
+        cy.get('#error-page a').click();
+
+        // assert login form exists and assert its elements
+        cy.get('#login').should('exist');
+        cy.get('.headLabel').should('have.text', 'Single sign on service');
+        cy.get('.commentLabel').should('have.text', 'Use your domain username and password');
+        cy.get('#username').should('have.class', 'required').and('have.attr', 'placeholder', 'Username');
+        cy.get('#password').should('have.class', 'required').and('have.attr', 'placeholder', 'Password');
+        cy.get('.btn-submit').invoke('val').should('eq', 'Login');
+    });
+
+    it('Failed login test', () => {
+        cy.get('#username').type('username');
+        cy.get('#password').type('password');
+        cy.contains('Login').click();
+
+        // assert login form exists and assert error message
+        cy.get('#login').should('exist');
+        cy.get('#login > form > #msg').should('have.text', 'Invalid credentials').and('have.class', 'errors');
+    });
+});
